ad421cb3737a4c22a9717f672df3ebedb1fc32ef
David <dmeyers@mesarim.com>
2025-07-26 14:06:51 -0700

    Re-organize scraper into distinct function calls

--- Scraper.py
+++ Scraper.py
@@ -6,41 +6,59 @@ from pymongo.server_api import ServerApi
 
 config = dotenv_values(".env") # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 
-url = 'https://ballchasing.com/api/'
-headers = {'Authorization' : config["BallChasingAPIKey"]}
-api_key = {'Public' : 'egreacgj', 'Private' : config["MongoDBAPIKey"]}
+# BallChasing API
+def get_replays(playlist):
+	bc_url = 'https://ballchasing.com/api/'
+	bc_headers = {'Authorization' : config["BallChasingAPIKey"]}
 
-replays_url = f"{url}replays"
-params = {'playlist':'ranked-doubles'}
-response = requests.get(replays_url, headers=headers, params=params)
-print(response.status_code)
-data = json.loads(response.text)
-print(len(data['list']))
-first_match = data['list'][0]
-print(json.dumps(first_match, indent=2))
-replay = first_match['id']
-replay_url = f"{replays_url}/{replay}"
-response = requests.get(replay_url, headers=headers)
-game = json.loads(response.text)
-print(json.dumps(game, indent=2))
+	replays_url = f"{bc_url}replays"
+	params = {'playlist':playlist}
+	response = requests.get(replays_url, headers=bc_headers, params=params)
+	print(response.status_code)
+	if response.status_code >= 500:
+		print('Error getting replay meta data from BC')
+		print(response)
+		exit()
+	data = json.loads(response.text)
+	print(len(data['list']))
+	first_match = data['list'][0]
+	print(json.dumps(first_match, indent=2))
+	replay = first_match['id']
+	replay_url = f"{replays_url}/{replay}"
+	response = requests.get(replay_url, headers=bc_headers)
+	if response.status_code >= 500:
+		print('Error getting replay data from BC')
+		print(response)
+		exit()
+	game = json.loads(response.text)
+	print(json.dumps(game, indent=2))
+	return data
 
-uri = config["MongoDBURI"]
+# MongoDB
+def insert_replays(data):
+	db_uri = config["MongoDBURI"]
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-database = client['RankedDoubles']
-database.create_collection('ReplayMetaData')
-collection = database['ReplayMetaData']
+	# Create a new client and connect to the server
+	client = MongoClient(db_uri, server_api=ServerApi('1'))
+	database = client['RankedDoubles']
+	if 'ReplayMetaData' not in database.list_collection_names():
+		database.create_collection('ReplayMetaData')
+	collection = database['ReplayMetaData']
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+	# Send a ping to confirm a successful connection
+	try:
+		client.admin.command('ping')
+		print("Pinged your deployment. You successfully connected to MongoDB!")
+	except Exception as e:
+		print(e)
 
-try:
-    collection.insert_many(data['list'], ordered=False)
-    print('Insert Successful!')
-except Exception as e:
-    print(e)
+	try:
+		collection.insert_many(data['list'], ordered=False)
+		print('Insert Successful!')
+	except Exception as e:
+		print(e)
+
+if __name__ == "__main__":
+	replays = get_replays('ranked-doubles')
+	insert_replays(replays)
+	print("Success")
